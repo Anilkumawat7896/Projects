167988cbe0d7247391b82a6260c568843f58ab6b
Anil <anilkumawat7896@gmail.com>
2023-12-06 10:04:24 +0530

    file/folder structure is compelete

--- SnapGram/src/App.tsx
+++ SnapGram/src/App.tsx
@@ -1,13 +1,23 @@
 import "./globals.css";
 import { Routes, Route } from "react-router-dom";
+import SingInForm from "./_auth/forms/SingInForm";
+import SingUpForm from "./_auth/forms/SingUpForm";
+import AuthLayout from "./_auth/AuthLayout";
+import { Home } from "./_root/pages";
+import RootLayout from "./_root/RootLayout";
 function App() {
   return (
     <main className="flex h-screen">
       <Routes>
         {/* public routes  */}
-        <Route path="/sing-in" element={<SinginForm />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/sing-in" element={<SingInForm />} />
+          <Route path="/sing-in" element={<SingUpForm />} />
+        </Route>
         {/* Private routes  */}
-        <Route index element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   );
---
+++ SnapGram/src/_auth/forms/SingUpForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SingUpForm = () => {
+  return <div>Sing Up Form</div>;
+};
+
+export default SingUpForm;
